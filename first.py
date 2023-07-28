c9db138ddf1929fc88cae3f1597ce166c322a78d
Shivank Shukla <shuklashivank28@gmail.com>
2023-07-28 15:12:56 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,13 @@
+while True:
+    a = int(input(" Enter number 1 = "))
+
+    b= int(input(" Enter number 2 = "))
+
+    z = a*b
+
+    print( " your answer is : ", z)
+
+    x=input("Wannna try again y/n ")
+    if x== "n" :
+        print("Thank you for using it")
+        break
